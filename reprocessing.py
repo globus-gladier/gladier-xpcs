5aeb8e00ca17dc4f6cc1c68763c2c129c4170341
Ryan <rchard@anl.gov>
2021-03-30 15:57:56 -0500

    Include transfer of qmap file.

--- reprocessing.py
+++ reprocessing.py
@@ -29,18 +29,21 @@ if __name__ == '__main__':
         'input': {
             # Manifest input files, and destination for where to process (Anyone can use the manifest below)
             'manifest_id': '8b5c50ff-838d-4072-ad6c-ce9d142d6b04',
-            'manifest_destination': 'globus://08925f04-569f-11e7-bef8-22000b9a448b/projects/APSDataAnalysis/Automate/reprocessing/',
+            'manifest_destination': 'globus://08925f04-569f-11e7-bef8-22000b9a448b/projects/APSDataAnalysis/Automate_ryan/reprocessing/',
 
             # General info info
             'corr_loc': '/lus/theta-fs0/projects/APSDataAnalysis/XPCS/xpcs-eigen/build/corr',
             'flags': '',
-            'qmap_file': 'sanat201903_qmap_S270_D54_lin.h5',
+            'qmap_source_endpoint': 'e55b4eab-6d04-11e5-ba46-22000b92c6ec',
+            'qmap_source_path': '/XPCSDATA/Automate/qmap/sanat201903_qmap_S270_D54_lin.h5',
+            'qmap_destination_endpoint': '08925f04-569f-11e7-bef8-22000b9a448b',
+            'qmap_file': '/projects/APSDataAnalysis/Automate_ryan/reprocessing/sanat201903_qmap_S270_D54_lin.h5',
             'flat_file': 'Flatfiel_AsKa_Th5p5keV.hdf',
-            'reprocessing_suffix': '_nick_reprocessing_test',
+            'reprocessing_suffix': '_ryan_reprocessing_test',
 
-            # Execution Environment (For Nick only).
-            'funcx_endpoint_non_compute': '0a162a09-8bd9-4dd9-b046-0bfd635d38a7',
-            'funcx_endpoint_compute': '37e6099f-e9e7-4817-ac68-4afcd78d8221',
+            # Execution Environment (For Ryan only).
+            'funcx_endpoint_non_compute': '6c4323f4-a062-4551-a883-146a352a43f5',
+            'funcx_endpoint_compute': '9f84f41e-dfb6-4633-97be-b46901e9384c',
 
             # Needed for running on gladier_tools directly, otherwise not used
             'proc_dir': '/lus/theta-fs0/projects/APSDataAnalysis/nick/xpcs',
