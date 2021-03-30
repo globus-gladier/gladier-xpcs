c5997692043f44d43c06a09777a9e40869c29263
Nickolaus D. Saint <nick@globus.org>
2021-03-30 14:16:38 -0700

    Add setup.py, move files into package dir
    This is so the reprocessing flow can be installed on the portal

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+
+install_requires = []
+with open('requirements.txt') as reqs:
+    for line in reqs.readlines():
+        req = line.strip()
+        if not req or req.startswith('#'):
+            continue
+        install_requires.append(req)
+
+setup(
+    name='xpcs-client',
+    description='The XPCS Gladier flow',
+    url='https://github.com/globus-gladier/xpcs_client',
+    maintainer='The Gladier Team',
+    maintainer_email='',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=install_requires,
+    dependency_links=[],
+    license='Apache 2.0',
+    classifiers=[]
+)
