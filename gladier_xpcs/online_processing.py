bf00ce134518d310a00cef7d0051f6757caaf70a
Rafael Vescovi <ravescovi@gmail.com>
2021-07-21 18:02:00 -0500

    fixes

--- gladier_xpcs/online_processing.py
+++ gladier_xpcs/online_processing.py
@@ -5,17 +5,18 @@ from gladier import GladierBaseClient, generate_flow_definition
     'pre_publish_gather_metadata': {'WaitTime': 1200},
     'eigen_corr': {'WaitTime': 3600},
     'make_corr_plots':{'WaitTime': 3600},
-    'publish_gather_metadata': {'payload': '$.GatherXpcsMetadata.details.result[0]'}
+#    'publish_gather_metadata': {'payload': '$.GatherXpcsMetadata.details.result[0]'}
 })
 class XPCSClient(GladierBaseClient):
     globus_group = '368beb47-c9c5-11e9-b455-0efb3ba9a670'
 
     gladier_tools = [
-        'gladier_xpcs.tools.transfer_from_clutch_to_theta.TransferFromClutchToTheta',
-        'gladier_xpcs.tools.pre_publish.PrePublish',
+        'gladier_xpcs.tools.TransferFromClutchToTheta',
+        'gladier_xpcs.tools.PrePublish',
         'gladier_xpcs.tools.EigenCorr',
         # 'gladier_xpcs.tools.transfer_from_clutch_to_theta.TransferToClutch',
-        'gladier_xpcs.tools.plot.MakeCorrPlots',
+        'gladier_xpcs.tools.MakeCorrPlots',
         'gladier_xpcs.tools.gather_xpcs_metadata.GatherXPCSMetadata',
-        'gladier_tools.publish.Publish',
+        'gladier_xpcs.tools.Publish',
     ]
+
