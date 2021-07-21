4d3edb6deb7cdd9b47b20be0667c3f28a7ae5cbc
Rafael Vescovi <ravescovi@gmail.com>
2021-07-21 18:02:07 -0500

    fixes

--- gladier_xpcs/tools/__init__.py
+++ gladier_xpcs/tools/__init__.py
@@ -1,6 +1,22 @@
+from .transfer_from_clutch_to_theta import TransferFromClutchToTheta
+from .pre_publish import PrePublish
 from .corr import EigenCorr
+from .plot import MakeCorrPlots
+from .gather_xpcs_metadata import GatherXPCSMetadata
+from .publish import Publish
+
 from .apply_qmap import ApplyQmap
 from .custom_pilot import CustomPilot
-from .plot import MakeCorrPlots
 
-__all__ = ['EigenCorr', 'ApplyQmap', 'CustomPilot', 'MakeCorrPlots']
+
+
+__all__ = [
+    'TransferFromClutchToTheta',
+    'PrePublish',
+    'EigenCorr', 
+    'MakeCorrPlots',
+    'GatherXPCSMetadata'
+    'Publish',
+    'ApplyQmap', 
+    'CustomPilot', 
+    ]
