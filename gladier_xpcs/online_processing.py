4851242210529bfec06041b96e31176a4c08b85e
ravescovi <ravescovi@gmail.com>
2021-07-22 13:50:10 -0500

    fix publish

--- gladier_xpcs/online_processing.py
+++ gladier_xpcs/online_processing.py
@@ -5,7 +5,7 @@ from gladier import GladierBaseClient, generate_flow_definition
     'pre_publish_gather_metadata': {'WaitTime': 1200},
     'eigen_corr': {'WaitTime': 7200},
     'make_corr_plots':{'WaitTime': 7200},
-#    'publish_gather_metadata': {'payload': '$.GatherXpcsMetadata.details.result[0]'}
+    'publish_gather_metadata': {'payload': '$.GatherXpcsMetadata.details.result[0]'}
 })
 class XPCSClient(GladierBaseClient):
     globus_group = '368beb47-c9c5-11e9-b455-0efb3ba9a670'
