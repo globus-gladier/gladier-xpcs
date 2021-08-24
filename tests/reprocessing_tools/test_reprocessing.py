30e1bce4008ae54d7587846f3f5ca759c7856d39
Nickolaus D. Saint <nick@globus.org>
2021-08-24 16:43:26 -0700

    Added tests
    Added tests using pytest. Run using
    $ pip install pytest
    $ pytest

---
+++ tests/reprocessing_tools/test_reprocessing.py
@@ -0,0 +1,33 @@
+from gladier_xpcs.client_reprocess import XPCSReprocessingClient
+from gladier_xpcs.reprocessing_tools.publish_preparation import publish_preparation
+
+def test_reprocessing_get_xpcs_input(reprocessing_deployment, reprocessing_input):
+    """Make sure input is being built correctly from deployments"""
+    cli = XPCSReprocessingClient()
+    fi = cli.get_xpcs_input(reprocessing_deployment,
+                            reprocessing_input['hdf_source'],
+                            reprocessing_input['imm_source'],
+                            reprocessing_input['qmap_source_path'],
+                            )
+    assert fi['input']['hdf_file'] == (
+        '/projects/APSDataAnalysis/xpcs/mock_processing_dir/'
+        'A001_Aerogel_1mm_att6_Lq0_001_0001-1000/A001_Aerogel_1mm_att6_Lq0_001_0001-1000.hdf'
+    )
+    assert fi['input']['imm_file'] == (
+        '/projects/APSDataAnalysis/xpcs/mock_processing_dir/'
+        'A001_Aerogel_1mm_att6_Lq0_001_0001-1000/A001_Aerogel_1mm_att6_Lq0_001_00001-01000.imm'
+    )
+
+
+def test_publish_preparation(mock_pathlib, reprocessing_runtime_input):
+    from pprint import pprint
+    pprint(reprocessing_runtime_input)
+    result = publish_preparation(**reprocessing_runtime_input)
+    assert result['hdf_file'] == (
+        '/projects/APSDataAnalysis/xpcs/mock_processing_dir/'
+        'A001_Aerogel_1mm_att6_Lq0_001_0001-1000<qmap_suffix>/A001_Aerogel_1mm_att6_Lq0_001_0001-1000<qmap_suffix>.hdf'
+    )
+    assert result['pilot']['dataset'] == (
+        '/projects/APSDataAnalysis/xpcs/mock_processing_dir/'
+        'A001_Aerogel_1mm_att6_Lq0_001_0001-1000<qmap_suffix>'
+    )
