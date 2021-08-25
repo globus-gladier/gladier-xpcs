101bda44f83bf902ede95bd1ef900edf16655bf5
Raf <ravescovi@gmail.com>
2021-08-25 16:11:46 -0500

    dev

--- gladier_xpcs/deployments.py
+++ gladier_xpcs/deployments.py
@@ -19,13 +19,31 @@ class TalcDeployment(BaseDeployment):
     }
 
     funcx_endpoints = {
-        'funcx_endpoint_non_compute': '553e7b64-0480-473c-beef-be762ba979a9',
-        'funcx_endpoint_compute': '2272d362-c13b-46c6-aa2d-bfb22255f1ba',
+        'funcx_endpoint_non_compute': 'e449e8b8-e114-4659-99af-a7de06feb847',
+        'funcx_endpoint_compute': '4c676cea-8382-4d5d-bc63-d6342bdb00ca',
     }
 
     flow_input = {
         'input': {
             'staging_dir': '/eagle/APSDataAnalysis/XPCS/data_online',
+        }
+    }
+
+class RafCooleyDeployment(BaseDeployment):
+
+    globus_endpoints = {
+        'globus_endpoint_source': 'fdc7e74a-fa78-11e8-9342-0e3d676669f4',
+        'globus_endpoint_proc': '08925f04-569f-11e7-bef8-22000b9a448b',
+    }
+
+    funcx_endpoints = {
+        'funcx_endpoint_non_compute': '1d5c6081-d716-4b94-b00b-661409876688',
+        'funcx_endpoint_compute':     'f8f611a1-0c45-4bf6-87c5-e85b9fb4d7c0',
+    }
+
+    flow_input = {
+        'input': {
+            'staging_dir': '/eagle/APSDataAnalysis/XPCS_test/cooley_raf',
         }
     }
 
@@ -53,5 +71,6 @@ class NickDeployment(BaseDeployment):
 
 deployment_map = {
     'talc-prod': TalcDeployment(),
+    'raf-cooley': RafCooleyDeployment(),
     'nick-testing': NickDeployment(),
 }
--- scripts/raf_dev.py
+++ scripts/raf_dev.py
@@ -3,18 +3,20 @@
 ## /home/beams/8IDIUSER/.conda/envs/gladier/bin/python /home/beams10/8IDIUSER/DM_Workflows/xpcs8/automate/raf/gladier-xpcs/scripts/xpcs_corr_client.py --hdf '/data/xpcs8/2019-1/comm201901/cluster_results/A001_Aerogel_1mm_att6_Lq0_001_0001-1000.hdf' --imm /data/xpcs8/2019-1/comm201901/A001_Aerogel_1mm_att6_Lq0_001/A001_Aerogel_1mm_att6_Lq0_001_00001-01000.imm --group 0bbe98ef-de8f-11eb-9e93-3db9c47b68ba
 
 # Enable Gladier Logging
-import gladier.tests
+#import gladier.tests
 
-from gladier_xpcs.client_online_corr import XPCSClient
 import argparse
 import os
+
+from gladier_xpcs.flow_online import XPCSOnlineFlow
+from gladier_xpcs.deployments import deployment_map
 
 
 def register_container():
     from funcx.sdk.client import FuncXClient
     fxc = FuncXClient()
     from gladier_xpcs.tools.corr import eigen_corr
-    cont_dir = '/eagle/APSDataAnalysis/XPCS_test/containers/'
+    cont_dir = '/eagle/APSDataAnalysis/XPCS/containers/'
     container_name = 'eigen_v2.simg'
     eigen_cont_id = fxc.register_container(location=cont_dir+container_name,container_type='singularity')
     corr_cont_fxid = fxc.register_function(eigen_corr, container_uuid=eigen_cont_id)
@@ -30,24 +32,25 @@ def arg_parse():
                         default='/data/xpcs8/2019-1/comm201901/A001_Aerogel_1mm_att6_Lq0_001'
                                 '/A001_Aerogel_1mm_att6_Lq0_001_00001-01000.imm')
     parser.add_argument('--group', help='Visibility in Search', default=None)
-    parser.add_argument('--source-globus-ep', help='Source Globus Endpoint (Default Clutch)',
-                        default='fdc7e74a-fa78-11e8-9342-0e3d676669f4')
-    parser.add_argument('--compute-globus-ep', help='Compute Globus Endpoint (Default Theta)',
-                        default='08925f04-569f-11e7-bef8-22000b9a448b')
-    parser.add_argument('--processing-dir', help='Location folder on compute endpoint to process data',
-                        default='/eagle/APSDataAnalysis/XPCS_test')
-
+    parser.add_argument('--deployment','-d', default='talc-prod', help=f'Deployment configs. Available: {list(deployment_map.keys())}')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
 
     args = arg_parse()
+
+    depl = deployment_map.get(args.deployment)
+    if not depl:
+        raise ValueError(f'Invalid Deployment, deployments available: {list(deployment_map.keys())}')
+
+    depl_input = depl.get_input()
+
     hdf_name = os.path.basename(args.hdf)
     imm_name = os.path.basename(args.imm)
 
     input_parent_dir = hdf_name.replace('.hdf', '')
-    dataset_dir = os.path.join(args.processing_dir, input_parent_dir)
+    dataset_dir = os.path.join(depl_input['input']['staging_dir'], input_parent_dir)
 
     # Generate Destination Pathnames.
     hdf_file = os.path.join(dataset_dir, hdf_name)
@@ -60,7 +63,7 @@ if __name__ == '__main__':
                 'dataset': dataset_dir,
                 'index': '6871e83e-866b-41bc-8430-e3cf83b43bdc',
                 'project': 'xpcs-8id',
-                'source_globus_endpoint': args.compute_globus_ep,
+                'source_globus_endpoint': depl_input['input']['globus_endpoint_proc'],
                 # Extra groups can be specified here. The XPCS Admins group will always
                 # be provided automatically.
                 'groups': [args.group] if args.group else [],
@@ -85,13 +88,13 @@ if __name__ == '__main__':
 
             # funcX endpoints
             # Should think of moving those to a cfg with better naming
-            'funcx_endpoint_non_compute':'e449e8b8-e114-4659-99af-a7de06feb847',
-            'funcx_endpoint_compute':    '4c676cea-8382-4d5d-bc63-d6342bdb00ca',
+            'funcx_endpoint_non_compute': depl_input['input']['funcx_endpoint_non_compute'],
+            'funcx_endpoint_compute': depl_input['input']['funcx_endpoint_compute'],
 
 
             # globus endpoints
-            'globus_endpoint_clutch': args.source_globus_ep,
-            'globus_endpoint_theta': args.compute_globus_ep,
+            'globus_endpoint_clutch': depl_input['input']['globus_endpoint_source'],
+            'globus_endpoint_theta': depl_input['input']['globus_endpoint_proc'],
 
             # container hack for corr 
             'eigen_corr_funcx_id': register_container()
@@ -99,13 +102,12 @@ if __name__ == '__main__':
     }
 
 
-    corr_cli = XPCSClient()
+    corr_cli = XPCSOnlineFlow()
 
-    corr_flow_label = hdf_name
-    #print(corr_flow_label)
+    corr_flow_label = hdf_name + '_dev'
+
     corr_flow = corr_cli.run_flow(flow_input=flow_input, label=corr_flow_label)
 
-    #print('flow_id : ' + corr_cli.get_flow_id)
     print('run_id : ' + corr_flow['action_id'])
 
     #import pprint
