498641261b1e3814805f0fdde0b60b4fd6eaa156
hparraga <hparraga@anl.gov>
2022-02-03 11:50:40 -0600

    remove outdated tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ from unittest.mock import Mock
 import gladier
 from gladier_xpcs.flow_reprocess import XPCSReprocessingFlow
 from gladier_xpcs.deployments import BaseDeployment
-from gladier_xpcs.tools import xpcs_metadata
 
 
 
@@ -27,15 +26,6 @@ def mock_pathlib(monkeypatch):
         return path if isinstance(path, pathlib.Path) else pathlib.Path(path)
     monkeypatch.setattr(pathlib.Path, 'rename', rename)
     return pathlib.Path
-
-
-@pytest.fixture
-def mock_gather(monkeypatch):
-    monkeypatch.setattr(xpcs_metadata, 'gather', Mock())
-    xpcs_metadata.gather.return_value = {
-        'measurement.instrument.acquisition.root_folder': '/data/2020-1/sanat202002/',
-    }
-    return xpcs_metadata.gather
 
 
 @pytest.fixture
