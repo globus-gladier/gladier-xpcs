c1484671b4ae94ebb95a0c54c8fd5df7556538c5
Nickolaus D. Saint <nick@globus.org>
2022-03-03 09:52:48 -0800

    refactor: Fix inputs for testing xpcs_boost_corr directly

--- gladier_xpcs/tools/xpcs_boost_corr.py
+++ gladier_xpcs/tools/xpcs_boost_corr.py
@@ -3,9 +3,9 @@ from gladier import GladierBaseTool, generate_flow_definition
 def xpcs_boost_corr(**data):
     import os
     import json
-    from boost_corr.xpcs_aps_8idi.gpu_corr_multitau import solve_multitau, solve_twotime
+    from boost_corr.xpcs_aps_8idi import gpu_corr_multitau, gpu_corr_twotime
 
-    if not os.path.exists(data['proc_dic']):
+    if not os.path.exists(data['proc_dir']):
         raise NameError(f'{data["proc_dir"]} \n Proc dir does not exist!')
 
     os.chdir(data['proc_dir'])
@@ -14,9 +14,9 @@ def xpcs_boost_corr(**data):
 
 
     if atype in ('Multitau', 'Both'):
-        solve_multitau(**data['boost_corr'])
+        gpu_corr_multitau.solve_multitau(**data['boost_corr'])
     elif atype in ('Twotime', 'Both'):
-        solve_twotime(**data['boost_corr'])
+        gpu_corr_twotime.solve_twotime(**data['boost_corr'])
 
     return_values = {}
     # These are assumed by the boost_corr client. Look for them ad append them to output
@@ -47,10 +47,26 @@ class BoostCorr(GladierBaseTool):
 
 
 if __name__ == '__main__':
-    data = {'proc_dir':'/eagle/APSDataAnalysis/raf/xpcs_gpu',
-    'raw_file':'C032_B315_A200_150C_att01_001_0001-1000/input/C032_B315_A200_150C_att01_001_00001-01000.imm',
-    'qmap_file':'C032_B315_A200_150C_att01_001_0001-1000/qmap/bates202202_qmap_Lq1_ccdz25_S270_D54.h5',
-    'verbose':True}
+    data = {
+        'proc_dir':'/eagle/APSDataAnalysis/nick/xpcs_gpu',
+        'boost_corr': {
+            'raw':'C032_B315_A200_150C_att01_001_0001-1000/input/C032_B315_A200_150C_att01_001_00001-01000.imm',
+            'qmap':'C032_B315_A200_150C_att01_001_0001-1000/qmap/bates202202_qmap_Lq1_ccdz25_S270_D54.h5',
+            'verbose':True,
+            'atype': 'Both',
+
+            'output': 'C032_B315_A200_150C_att01_001_0001-1000/qmap/',
+            'batch_size': 8,
+            'gpu_id': 0,
+            'verbose': True,
+            'masked_ratio_threshold': 0.75,
+            'use_loader': True,
+            'begin_frame': 1,
+            'end_frame': -1,
+            'avg_frame': 1,
+            'stride_frame': 1,
+            'overwrite': False
+        }
+    }
     
     xpcs_boost_corr(**data)
-
