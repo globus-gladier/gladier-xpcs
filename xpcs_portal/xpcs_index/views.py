090047ad6a68f9460cda85c4e222794f0aee128b
Nickolaus D. Saint <nick@globus.org>
2023-12-14 13:43:59 -0800

    chore: Run black on views.py

--- xpcs_portal/xpcs_index/views.py
+++ xpcs_portal/xpcs_index/views.py
@@ -32,7 +32,7 @@ class XPCSSearchView(LoginRequiredMixin, PaginatedSearchView, SearchView):
 
     @property
     def filters(self):
-        return super().filters + self.get_index_info().get('default_filters', [])
+        return super().filters + self.get_index_info().get("default_filters", [])
 
 
 class XPCSDetailView(LoginRequiredMixin, DetailView):
@@ -41,19 +41,20 @@ class XPCSDetailView(LoginRequiredMixin, DetailView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         preview_list = (
-            'all_preview',
-            'correlation_plot_previews',
-            'correlation_plot_with_fit_previews',
-            'intensity_plot_previews',
-            'structural_analysis_prev',
-            'text_outputs',
+            "all_preview",
+            "correlation_plot_previews",
+            "correlation_plot_with_fit_previews",
+            "intensity_plot_previews",
+            "structural_analysis_prev",
+            "text_outputs",
         )
         try:
             for preview in preview_list:
                 for manifest in context.get(preview, []):
-                    match = FilenameFilter.match(self.request.user,
-                                                 manifest.get('filename'))
-                    manifest['show_filename'] = match
+                    match = FilenameFilter.match(
+                        self.request.user, manifest.get("filename")
+                    )
+                    manifest["show_filename"] = match
         except Exception as e:
             log.exception(e)
         return context
@@ -62,29 +63,34 @@ class XPCSDetailView(LoginRequiredMixin, DetailView):
 class XPCSReprocessing(object):
     """Reprocessing Checkout starts the flow immediately on verifying
     each of the subject it can process are valid"""
+
     form_class = ReprocessDatasetsCheckoutForm
-    template_name = 'xpcs/reprocess-datasets-checkout.html'
-    flow = '72e6469a-cf30-46bc-bff4-94dca46f2459'
+    template_name = "xpcs/reprocess-datasets-checkout.html"
+    flow = "72e6469a-cf30-46bc-bff4-94dca46f2459"
     authorization_type = "CUSTOM"
     group = "368beb47-c9c5-11e9-b455-0efb3ba9a670"
     # The auth key is set dynamically through the form by get_flow_authorization instead
     # authorization_key = "aps8idi-polaris"
 
-    def get_flow_authorization(self, authorization_type: str, authorization_key: str, form: forms.Form = None) -> FlowAuthorization:
+    def get_flow_authorization(
+        self, authorization_type: str, authorization_key: str, form: forms.Form = None
+    ) -> FlowAuthorization:
         akey = {
-            'aps8idi-polaris': 'CONFIDENTIAL_CLIENT',
-            'aps8idi-polaris-backup': 'USER'
+            "aps8idi-polaris": "CONFIDENTIAL_CLIENT",
+            "aps8idi-polaris-backup": "USER",
         }
-        return super().get_flow_authorization(akey[form.cleaned_data['facility']], form.cleaned_data['facility'], form)
+        return super().get_flow_authorization(
+            akey[form.cleaned_data["facility"]], form.cleaned_data["facility"], form
+        )
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['index'] = self.kwargs['index']
+        context["index"] = self.kwargs["index"]
         return context
 
     def get_success_url(self):
-        url = reverse_lazy('search', kwargs={'index': 'xpcs'})
-        return f'{url}?flow={self.flow}'
+        url = reverse_lazy("search", kwargs={"index": "xpcs"})
+        return f"{url}?flow={self.flow}"
 
 
 class XPCSReprocessingSearchReprocessing(XPCSReprocessing, BatchCreateView):
@@ -97,23 +103,31 @@ class XPCSReprocessingSearchReprocessing(XPCSReprocessing, BatchCreateView):
 
 
 class XPCSComputeTransfer(XPCSReprocessing, BatchCreateView):
-    template_name = 'xpcs/transfer-selection-form.html'
+    template_name = "xpcs/transfer-selection-form.html"
     form_class = CollectionSelectionForm
     collector = XPCSTransferCollector
     collection = "74defd5b-5f61-42fc-bcc4-834c9f376a4f"
     path = "/XPCSDATA/"
-    cycle_exclude = ["spec_data", "partitionMapLibrary", "MDF", "AutomateTesting", "Automate"]
+    cycle_exclude = [
+        "spec_data",
+        "partitionMapLibrary",
+        "MDF",
+        "AutomateTesting",
+        "Automate",
+    ]
     dataset_exclude = ["ALCF_results", "cluster_results", "logs"]
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['index'] = self.kwargs['index']
-        context.update({
-            'collection': self.collection,
-            'path': self.path,
-            'cycle_exclude': self.cycle_exclude,
-            'dataset_exclude': self.dataset_exclude,
-        })
+        context["index"] = self.kwargs["index"]
+        context.update(
+            {
+                "collection": self.collection,
+                "path": self.path,
+                "cycle_exclude": self.cycle_exclude,
+                "dataset_exclude": self.dataset_exclude,
+            }
+        )
         return context
 
     def get_collector(self, collection: str, path: str) -> XPCSTransferCollector:
@@ -130,7 +144,11 @@ class XPCSComputeTransfer(XPCSReprocessing, BatchCreateView):
         authorization = self.get_flow_authorization(
             self.authorization_type, self.authorization_key, form=form
         )
-        path = pathlib.Path(self.path) / form.cleaned_data['cycle'] / form.cleaned_data['parent']
+        path = (
+            pathlib.Path(self.path)
+            / form.cleaned_data["cycle"]
+            / form.cleaned_data["parent"]
+        )
         collector = self.get_collector(self.collection, str(path))
         collector.save()
         batch = self.get_batch(authorization, collector, form)
