2fa615c70e8c636ac5b959689376cdce934e9c44
Hannah Parraga <hparraga@anl.gov>
2025-03-17 13:44:46 -0500

    restore permissions for nested directories

--- scripts/dm/permissions.sh
+++ scripts/dm/permissions.sh
@@ -7,9 +7,8 @@ WORKFLOW_SETUP_FILE=${1:-/home/dm/etc/dm.workflow_setup.sh}
 START_TIME=$(date +%s.%N)
 source $WORKFLOW_SETUP_FILE
 END_TIME=$(date +%s.%N)
-TIME_DIFF=$(awk "BEGIN {print $END_TIME - $START_TIME}")
-TIME_DIFF=$(printf "%.2f" "$TIME_DIFF")
-echo "Time sourcing setup script: $TIME_DIFF seconds"
+TIME_DIFF=$((END_TIME - START_TIME))
+echo "Time sourcing setup file: $TIME_DIFF seconds"
 
 EXPERIMENT_NAME=$2
 if [[ "$#" -lt 2 || "$EXPERIMENT_NAME" == "" ]]; then
@@ -19,10 +18,43 @@ if [[ "$#" -lt 2 || "$EXPERIMENT_NAME" == "" ]]; then
 fi
 
 RESULT_PATH=${3:-analysis/}
+IFS='/' read -ra SUBDIRS <<< "$RESULT_PATH"
+# Check if any of the subdirectories were created during
+# analysis, and if so, restore permissions from there
+RESTORE_PATH=""
+for SUBDIR in "${SUBDIRS[@]}"; do
+    if [[ -n "$SUBDIR" ]]; then
+        RESTORE_PATH+="/$SUBDIR"
+        if [[ "$RESTORE_PATH" == *"analysis/"* ]]; then
+            OWNER=$(stat -c '%U' "$RESTORE_PATH" 2>/dev/null) 
+            if [ "$OWNER" != "dmadmin" ]; then
+                RELATIVE_RESTORE_PATH="analysis${RESTORE_PATH#*analysis}"
+                START_TIME=$(date +%s.%N)
+                dm-restore-permissions --relative-path $RELATIVE_RESTORE_PATH --experiment $EXPERIMENT_NAME
+                if [ $? != 0 ]; then
+                    echo "ERROR: Unable to restore permissions for experiment $EXPERIMENT_NAME path $RELATIVE_RESTORE_PATH" 
+                    exit 1
+                fi
+                END_TIME=$(date +%s.%N)
+                TIME_DIFF=$((END_TIME - START_TIME))
+                echo "Time restoring permissions: $TIME_DIFF seconds"
+                echo "Restored permissions for experiment $EXPERIMENT_NAME path $RELATIVE_RESTORE_PATH"
+                exit 0
+            fi
+        fi
+    fi
+done
 
-START_TIME=$(date +%s.%N)
-dm-restore-permissions --relative-path $RESULT_PATH --experiment $EXPERIMENT_NAME
-END_TIME=$(date +%s.%N)
-TIME_DIFF=$(awk "BEGIN {print $END_TIME - $START_TIME}")
-TIME_DIFF=$(printf "%.2f" "$TIME_DIFF")
+# New files in an existing directory
+RESTORE_PATH=$RESULT_PATH
+RELATIVE_RESTORE_PATH="analysis${RESTORE_PATH#*analysis}"
+START_TIME=$(date +%s)
+dm-restore-permissions --relative-path $RELATIVE_RESTORE_PATH --experiment $EXPERIMENT_NAME
+if [ $? != 0 ]; then
+    echo "ERROR: Unable to restore permissions for experiment $EXPERIMENT_NAME path $RELATIVE_RESTORE_PATH" 
+    exit 1
+fi
+END_TIME=$(date +%s)
+TIME_DIFF=$((END_TIME - START_TIME))
 echo "Time restoring permissions: $TIME_DIFF seconds"
+echo "Restored permissions for experiment $EXPERIMENT_NAME path $RELATIVE_RESTORE_PATH"
