a0152175201a5c008b3c72dc28332943d47e3825
Nickolaus D. Saint <nick@globus.org>
2025-03-18 12:28:57 -0700

    chore: Remove code no longer used in processing
    For way too long, we kept around the old versions of things instead of
    getting rid of them. These are far out of date and no longer useful at
    this point in time. Even reprocessing is far enough out of date, it doesn't
    serve a useful purpose.
    
    Init shortcuts also removed, as those weren't adding a whole lot and
    obfuscating the locations of modules.

--- gladier_xpcs/deployments.py
+++ gladier_xpcs/deployments.py
@@ -54,110 +54,6 @@ class Talc(BaseDeployment):
     }
 
 
-class NickPolarisGPU(BaseDeployment):
-
-    source_collection = xpcs_data
-    staging_collection = apsdataanalysis
-    pub_collection = xpcs_data
-
-    globus_endpoints = {
-        # Eagle -- XPCS Data 8-ID APS
-        'globus_endpoint_source': xpcs_data.uuid,
-        'globus_endpoint_proc': apsdataanalysis.uuid,
-    }
-
-    compute_endpoints = {
-        # Theta login
-        # 'login_node_endpoint': '553e7b64-0480-473c-beef-be762ba979a9',
-        # # Containers
-        # 'compute_endpoint': '4a6f2b52-d392-4a57-ad77-ae6e86daf503',
-        'login_node_endpoint': '2fe84982-1c50-45bf-8d68-e4b659c128cb',
-        'compute_endpoint': '2fe84982-1c50-45bf-8d68-e4b659c128cb',
-    }
-
-    function_ids = {
-        "acquire_nodes_function_id": "1f937cb4-7276-4867-bb9f-6c42031be090",
-        "xpcs_boost_corr_function_id": "d95600d7-2f5c-4453-8d25-655b8429e839",
-        "make_corr_plots_function_id": "7245d81b-4530-4ee8-b246-4ad043a71da2",
-        "gather_xpcs_metadata_function_id": "a6570e9f-69a3-470a-bc3d-39f87d794f82",
-        "publish_gather_metadata_function_id": "5756975d-0b08-4f5f-8314-1417c6e3db54"
-    }
-
-    flow_input = {
-        'input': {
-            'staging_dir': staging_collection.path / 'nick/xpcs_staging',
-        }
-    }
-
-
-class HannahTheta(BaseDeployment):
-
-    source_collection = xpcs_data
-    staging_collection = apsdataanalysis
-    pub_collection = xpcs_data
-
-    globus_endpoints = {
-        # Eagle -- XPCS Data 8-ID APS
-        'globus_endpoint_source': xpcs_data.uuid,
-        'globus_endpoint_proc': apsdataanalysis.uuid,
-    }
-
-    flow_input = {
-        'input': {
-            'staging_dir': staging_collection.path / 'hparraga/xpcs_staging',
-        }
-    }
-
-    compute_endpoints = {
-        'login_node_endpoint': 'e3e1aef6-0a6f-4ef1-b9c6-a14b0efb1dfa',
-        'compute_endpoint': '3d9fde8a-1dfa-4ce7-93ab-5d524a59a4f6',
-    }
-
-
-class HannahPolaris(BaseDeployment):
-    
-    source_collection = xpcs_data
-    staging_collection = apsdataanalysis
-    pub_collection = xpcs_data
-
-    globus_endpoints = {
-        # Eagle -- XPCS Data 8-ID APS
-        'globus_endpoint_source': xpcs_data.uuid,
-        'globus_endpoint_proc': apsdataanalysis.uuid, 
-    }
-
-    compute_endpoints = {
-        'login_node_endpoint': 'e3e1aef6-0a6f-4ef1-b9c6-a14b0efb1dfa',
-        'compute_endpoint': '0676a1f2-b92f-41f7-8e4f-6cc93eb6f929',
-    }
-
-    flow_input = {
-        'input': {
-            'staging_dir': staging_collection.path / 'hparraga/xpcs_staging',
-        }
-    }
-
-
-class RyanPolaris(BaseDeployment):
-
-    globus_endpoints = {
-        'globus_endpoint_source': 'e55b4eab-6d04-11e5-ba46-22000b92c6ec',
-        'globus_endpoint_proc': theta_ep.uuid,
-    }
-
-    compute_endpoints = {
-        'login_node_endpoint': '6c4323f4-a062-4551-a883-146a352a43f5',
-        'compute_endpoint': 'dc2a0cdb-2aee-44f7-a422-c4e28d9f7617',
-    }
-
-    flow_input = {
-        'input': {
-            'staging_dir': '/eagle/APSDataAnalysis/rchard/xpcs/',
-            'corr_loc': '/eagle/APSDataAnalysis/XPCS/xpcs-eigen/build/corr',
-        }
-    }
-
-
 class APS8IDIPolaris(BaseDeployment):
 
     source_collection = xpcs_data
@@ -274,35 +170,10 @@ class Voyager8IDIPolaris(BaseDeployment):
         }
     }
 
-class RafPolaris(BaseDeployment):
-
-    globus_endpoints = {
-        'globus_endpoint_source': 'e55b4eab-6d04-11e5-ba46-22000b92c6ec',
-        'globus_endpoint_proc': theta_ep.uuid,
-    }
-
-    compute_endpoints = {
-        'login_node_endpoint': 'e449e8b8-e114-4659-99af-a7de06feb847',
-        'compute_endpoint': 'a93b6438-6ff7-422e-a1a2-9a4c6d9c1ea5',
-    }
-
-    flow_input = {
-        'input': {
-            'staging_dir': '/eagle/APSDataAnalysis/XPCS/raf/xpcs/',
-        }
-    }
-
 deployment_map = {
     'talc-prod': Talc(),
-    'raf-polaris': RafPolaris(),
-    'hannah-theta': HannahTheta(),
-    'hannah-polaris': HannahPolaris(),
-    'ryan-polaris': RyanPolaris(),
-    'nick-polaris-gpu': NickPolarisGPU(),
     'aps8idi-polaris': APS8IDIPolaris(),
     'voyager-8idi-polaris': Voyager8IDIPolaris(),
     'voyager-xpcs8-polaris': VoyagerXPCS8Polaris(),
-    # This is a hack for the SC demo in case we can't get nodes. You can remove this after Nov 16th, 2023.
-    'aps8idi-polaris-backup': NickPolarisGPU(),
     'nersc': RyanNERSC(),
 }
--- gladier_xpcs/flows/flow_boost.py
+++ gladier_xpcs/flows/flow_boost.py
@@ -1,21 +1,4 @@
-"""
-XPCS Boost Flow
-
-Summary: This flow executes new xpcs_boost CPU/GPU flow.
-- Data is transfered from Clutch to Theta
-- An "empty" publication is added to the index
-- Nodes are "warmed"
-- Boost Multitau and/or Twotime is applied using CPU/GPU
-- Plots are made
-- Gather + Publish the final data to the portal
-"""
-import os
-from gladier import GladierBaseClient, generate_flow_definition, utils
-from gladier_xpcs.flows.container_flow_base import ContainerBaseClient
-from gladier_xpcs.deployments import deployment_map
-
-# import gladier_xpcs.log  # Uncomment for debug logging
-
+from gladier import GladierBaseClient, generate_flow_definition
 
 @generate_flow_definition(modifiers={
 #    'publishv2_gather_metadata': {
@@ -27,10 +10,10 @@ class XPCSBoost(GladierBaseClient):
     globus_group = '368beb47-c9c5-11e9-b455-0efb3ba9a670'
     
     gladier_tools = [
-        'gladier_xpcs.tools.SourceTransfer',
-        'gladier_xpcs.tools.BoostCorr',
-        'gladier_xpcs.tools.ResultTransfer',
-        'gladier_xpcs.tools.MakeCorrPlots',
+        'gladier_xpcs.tools.source_transfer.SourceTransfer',
+        'gladier_xpcs.tools.xpcs_boost_corr.BoostCorr',
+        'gladier_xpcs.tools.result_transfer.ResultTransfer',
+        'gladier_xpcs.tools.plot.MakeCorrPlots',
         'gladier_xpcs.tools.gather_xpcs_metadata.GatherXPCSMetadata',
         # Publication is currently broken due to the destination collection being down.
         # Uncomment this to re-enable
