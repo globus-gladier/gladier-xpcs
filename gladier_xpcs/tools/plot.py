351bd209838c8c1abd59ed8e6e2634238097d153
Nickolaus D. Saint <nick@globus.org>
2025-05-12 15:54:43 -0700

    fix: Plotting to include extra metadata
    This isn't the best solution, and likely another step to formulate this
    metadata file would be better

--- gladier_xpcs/tools/plot.py
+++ gladier_xpcs/tools/plot.py
@@ -5,16 +5,26 @@ import pathlib
 from gladier import GladierBaseTool, generate_flow_definition
 
 
-def make_corr_plots(hdf_file: str, webplot_target_dir: str, plotting_metadata_file: str = None, **data: dict):
+def make_corr_plots(hdf_file: str, webplot_target_dir: str, plotting_metadata_file: str = None, webplot_extra_metadata=None, **data: dict):
     import os
     import json
     import time
+    import pathlib
     from xpcs_webplot.plot_images import hdf2web_safe
     from xpcs_webplot import __version__ as webplot_version
 
     corr_start = time.time()
-    webplot_output = hdf2web_safe(hdf_file, target_dir=webplot_target_dir, image_only=True)
+    webplot_output = hdf2web_safe(hdf_file, target_dir=webplot_target_dir, image_only=True, overwrite=True)
     execution_time_seconds = round(time.time() - corr_start, 2)
+
+    # Update the metadata file to point to "project metadata" inside the schema. Add some extra stuff there.
+    with open(pathlib.Path(webplot_target_dir) / f"{pathlib.Path(hdf_file).stem}" / "metadata.json" , 'r+') as f:
+        plot_metadata = {"project_metadata": json.loads(f.read())}
+        if webplot_extra_metadata:
+            plot_metadata["project_metadata"].update(webplot_extra_metadata.get("project_metadata", {}))
+        f.seek(0)
+        f.write(json.dumps(plot_metadata, indent=4))
+        f.truncate()
 
     metadata = {
         'executable': {
