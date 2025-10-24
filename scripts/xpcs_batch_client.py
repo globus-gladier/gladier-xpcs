4d29593492e0b5acf71fb78afcdc0e15b5f72e83
Nickolaus D. Saint <nick@globus.org>
2025-10-24 16:03:39 -0700

    feat: Upgraded batch client to do batches of batches, so many batches
    This does include typer for separating things into commands.
    
    Still quite preliminary, but it's working for 800ish files!

--- scripts/xpcs_batch_client.py
+++ scripts/xpcs_batch_client.py
@@ -4,7 +4,9 @@ import urllib
 import pathlib
 import json
 import sys
+import functools
 import os
+import typer
 
 from gladier_xpcs.flows.flow_boost import XPCSBoost
 from gladier import FlowsManager
@@ -14,41 +16,97 @@ from gladier_xpcs.deployments import BaseDeployment, deployment_map
 
 from scripts import xpcs_online_boost_client
 
-batch_size = 225
-# Voyager
-deployment = deployment_map["voyager-8idi-polaris"]
-source_path = pathlib.Path(deployment.source_collection.to_globus("/8IDI/2025-2/tempus202507-merge/data/converted/Ha0277_PO2_a0002_f2000000/"))
-
-staging_dir = "batch-test-2025-10-22-batch-2/tempus202507-merge"
-staging_path = pathlib.Path(deployment.get_input()["input"]["staging_dir"]) / staging_dir
+globus_app = globus_sdk.ClientApp("scripting", client_id=os.getenv("GLOBUS_CLI_CLIENT_ID"), client_secret=os.getenv("GLOBUS_CLI_CLIENT_SECRET"))
+app = typer.Typer(no_args_is_help=True, pretty_exceptions_enable=False)
 
 # To run silently as nick only
 # run_kwargs = {"run_monitors": [f"urn:globus:auth:identity:3b843349-4d4d-4ef3-916d-2a465f9740a9"],
 #               "run_managers": [f"urn:globus:auth:identity:3b843349-4d4d-4ef3-916d-2a465f9740a9"]}
 run_kwargs = {}
 
-app = globus_sdk.ClientApp("scripting", client_id=os.getenv("GLOBUS_CLI_CLIENT_ID"), client_secret=os.getenv("GLOBUS_CLI_CLIENT_SECRET"))
-transfer_client = globus_sdk.TransferClient(app=app)
+def generate_batches_from_source(
+        source: str = "/8IDI/2025-2/tempus202507-merge/data/converted/Ha0277_PO2_a0002_f2000000/",
+        qmap: str = "/8IDI/2025-2/tempus202507-merge/data/timepix_Sq90_Dq9_log.hdf",
+        staging_dir: str = "batch-test-2025-10-22-batch-2/tempus202507-merge",
+        batch_size: int = 100,
+        limit: int = 0):
+
+    # Voyager
+    deployment = deployment_map["voyager-8idi-polaris"]
+    source_path = pathlib.Path(deployment.source_collection.to_globus(source))
+
+    # staging_dir = "batch-test-2025-10-22-batch-2/tempus202507-merge"
+    staging_path = pathlib.Path(deployment.get_input()["input"]["staging_dir"]) / staging_dir
+
+    transfer_client = globus_sdk.TransferClient(app=globus_app)
+    data = transfer_client.operation_ls(deployment.source_collection.uuid, path=source_path)
+    files = [d["name"] for d in data["DATA"] if d["type"] == "dir"]
+    if limit:
+        files = files[0:limit]
+        print(f"WARNING: Capped total amount of files to {limit}")
+    batches = [list(files[i: i + batch_size]) for i in range(0, len(files), batch_size)]
+
+    finalized_batches = list()
+    for idx, file_batch in enumerate(batches):
+
+        transfer_items = [
+            {
+                "source_path": str(source_path / item),
+                "destination_path": str(pathlib.Path(deployment.staging_collection.to_globus(staging_path)) / item / "input"),
+                "recursive": True
+            }
+            for item in file_batch
+        ]
+
+        qmap = pathlib.Path(qmap)
+        qmap_transfer_item = {
+            'destination_path': str(pathlib.Path(deployment.staging_collection.to_globus(staging_path)) / qmap.name),
+            'recursive': False,
+            'source_path': str(deployment.source_collection.to_globus(str(qmap))),
+        }
+        transfer_items.append(qmap_transfer_item)
+
+        # See if we can pack 100 datasets into a transfer
+        flow_input = {
+            "input": {
+                "compute_endpoint": "d88919ea-026a-493e-9124-fe3c46defa54",
+                "staging_base_path": str(staging_path),
+                "staging_qmap": qmap_transfer_item["destination_path"],
+                "boost_corr": get_boost_corr_defaults(),
+                "qmap": "/eagle/APSDataProcessing/aps8idi" + qmap_transfer_item["destination_path"],
+                "xpcs_boost_corr_tasks": [
+                    {
+                        "function_id": get_function_id("xpcs_boost_corr_function_id"),
+                        'kwargs': {'corr_input_file': "/eagle/APSDataProcessing/aps8idi/" + d["destination_path"]},
+                    } for d in transfer_items[:-1]
+                ],
+                'source_transfer': {
+                    'destination_endpoint_id': '98d26f35-e5d5-4edd-becf-a75520656c64',
+                    'source_endpoint_id': 'aa2b18e8-e248-4265-985c-7e2e59765539',
+                    'transfer_items': transfer_items
+                },
+            },
+        }
+
+        batch = {
+            "flow_id": None,
+            "flow_input": flow_input,
+            "batch_id": idx,
+            "batch_size": len(file_batch),
+        }
+        finalized_batches.append(batch)
+    return finalized_batches
 
 
-data = transfer_client.operation_ls(deployment.source_collection.uuid, path=source_path)
+@functools.lru_cache()
+def get_function_id(name: str):
+    flows_manager = FlowsManager(run_kwargs=run_kwargs)
+    batch_flow = XPCSBoostBatch(flows_manager=flows_manager)
+    fids = batch_flow.get_compute_function_ids()
+    if not fids.get("xpcs_boost_corr_function_id"):
+        print("WARNING: No xpcs_boost_corr_function_id function found!")
+    return fids[name]
 
-first_hundred = [d["name"] for d in data["DATA"] if d["type"] == "dir"][:batch_size]
-transfer_items = [
-    {
-        "source_path": str(source_path / item),
-        "destination_path": str(pathlib.Path(deployment.staging_collection.to_globus(staging_path)) / item / "input"),
-        "recursive": True
-    }
-    for item in first_hundred
-]
-
-qmap = {
-    'destination_path': str(pathlib.Path(deployment.staging_collection.to_globus(staging_path)) / "timepix_Sq90_Dq9_log.hdf"),
-    'recursive': False,
-    'source_path': str(deployment.source_collection.to_globus('/8IDI/2025-2/tempus202507-merge/data/timepix_Sq90_Dq9_log.hdf')),
-}
-transfer_items.append(qmap)
 
 
 def get_boost_corr_defaults():
@@ -78,53 +136,30 @@ def get_boost_corr_defaults():
         # "output": "",
     }
 
-from pprint import pprint 
-flows_manager = FlowsManager(run_kwargs=run_kwargs)
-batch_flow = XPCSBoostBatch(flows_manager=flows_manager)
-fids = batch_flow.get_compute_function_ids()
-# pprint(fids)
-if not fids.get("xpcs_boost_corr_function_id"):
-    print("WARNING: No xpcs_boost_corr_function_id function found!")
-# See if we can pack 100 datasets into a transfer
-flow_input = {
-    "input": {
-        # Old 8idi Polaris endpoint
-        # "compute_endpoint": "f8f4692a-0ab7-40d0-b256-ba5b82b5e2ec",
-        # Ryan's new ALCF MEP
-        "compute_endpoint": "d88919ea-026a-493e-9124-fe3c46defa54",
-        # Webplot xpcs
-        # "compute_endpoint": "48f5f3d6-dde5-4431-bb5f-8bc01e1cb386",
-        # Webplot gce-test3
-        # "compute_endpoint": "c66b4066-4a39-46ab-be95-216517d9234f",
-        # Tutorial (I think)
-        # "compute_endpoint": "4b116d3c-1703-4f8f-9f6f-39921e5864df",
-        "staging_base_path": str(staging_path),
-        "staging_qmap": qmap["destination_path"],
-        "boost_corr": get_boost_corr_defaults(),
-        "qmap": "/eagle/APSDataProcessing/aps8idi" + qmap["destination_path"],
-                #     {
-        #         'function_id.$': '$.input.xpcs_boost_corr_function_id',
-        #         'kwargs': {'dir.$': '$.input.source_transfer.transfer_items[0].destination_path'},
-        #     },
-        "xpcs_boost_corr_tasks": [
-            {
-                "function_id": fids["xpcs_boost_corr_function_id"],
-                'kwargs': {'corr_input_file': "/eagle/APSDataProcessing/aps8idi/" + d["destination_path"]},
-            } for d in transfer_items[:-1]
-        ],
-        'source_transfer': {
-            'destination_endpoint_id': '98d26f35-e5d5-4edd-becf-a75520656c64',
-            'source_endpoint_id': 'aa2b18e8-e248-4265-985c-7e2e59765539',
-            'transfer_items': transfer_items
-        },
-    },
-}
+@app.command()
+def generate_batches():
+    batches = generate_batches_from_source(limit=10, batch_size=1)
+    from pprint import pprint
+    pprint(batches)
 
-pprint(batch_flow.get_flow_definition())
-pprint(flow_input)
-label = f"{source_path.parts[3]}-test-{batch_size}-{QUEUE}"
-run = batch_flow.run_flow(flow_input=flow_input, label=label, tags=['aps', 'xpcs', 'batch-test'])
-status = batch_flow.progress(run["run_id"])
-pprint(batch_flow.get_status(run["run_id"]))
-pprint(status)
 
+@app.command()
+def run_batches():
+
+    flows_manager = FlowsManager(run_kwargs=run_kwargs)
+    batch_flow = XPCSBoostBatch(flows_manager=flows_manager)
+
+    batches = generate_batches_from_source(limit=0, batch_size=150)
+    for batch in batches:
+
+        # pprint(batch_flow.get_flow_definition())
+        # pprint(flow_input)
+        label = f"exp-test-{batch['batch_size']}-{QUEUE}-{batch['batch_id'] + 1}-of-{len(batches)}"
+        run = batch_flow.run_flow(flow_input=batch["flow_input"], label=label, tags=['aps', 'xpcs', 'batch-test'])
+        print(run["run_id"])
+        # status = batch_flow.progress(run["run_id"])
+        # pprint(batch_flow.get_status(run["run_id"]))
+        # pprint(status)
+
+if __name__ == "__main__":
+    app()
